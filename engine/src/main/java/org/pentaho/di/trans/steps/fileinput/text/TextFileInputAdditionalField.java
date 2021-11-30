bfcd9ec22074129344d8e1b8144d10cf47be9e38
ThinkFuture <ThinkFuture@aliyun.com>
2021-11-30 17:16:38 +0800

    TextFileInput 支持动态输出文件列

---
+++ engine/src/main/java/org/pentaho/di/trans/steps/fileinput/text/TextFileInputAdditionalField.java
@@ -0,0 +1,8 @@
+package org.pentaho.di.trans.steps.fileinput.text;/**
+ * @description TextFileInputAdditionalField
+ * @date 2021-11-26 17:41
+ * @author Jay
+ * @version v1.0.0.0
+ * @modifyRecord 
+ */public class TextFileInputAdditionalField {
+}
